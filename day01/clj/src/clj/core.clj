50f89a4b725eb85fff3f7c088651bfd1cb06e3fc
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-05-06 11:46:11 +0200

    Finish clojure day01

--- day01/clj/src/clj/core.clj
+++ day01/clj/src/clj/core.clj
@@ -1,9 +1,43 @@
 (ns clj.core
   (:gen-class))
 
-(defn -main
-  "Loop through `args` and convert string of numbers into a list of
-  integers"
-  [& args]
-  (doseq [arg args]
-    (println (map #(Character/getNumericValue %) arg))))
+(defn string-to-list-ints 
+  "Convert a string of ints into a list of ints"
+  [my-string]
+  (map #(Character/getNumericValue %) my-string))
+
+(defn compare-next [list-ints idx]
+  (= (nth list-ints idx)
+     (nth list-ints (mod (inc idx) (count list-ints)))))
+
+(defn compare-around [list-ints idx]
+  (= (nth list-ints idx)
+     (nth list-ints 
+          (mod (+ idx (/ 2 (count list-ints))) 
+               (count list-ints)))))
+
+(defn solve-captcha1 
+  "Computes the captcha for part 1 of day01"
+  [my-string]
+  (def list-ints (string-to-list-ints my-string))
+  (def ^:dynamic out)
+  (binding [out 0]
+    (dorun
+      (for [idx (range (count list-ints))]
+        (if (compare-next list-ints idx)
+          (set! out (+ out (nth list-ints idx))))))
+    out))
+ 
+(defn solve-captcha2
+  "Computes the captcha for part 2 of day01"
+  [my-string]
+  (def list-ints
+    (string-to-list-ints my-string))
+  (def ^:dynamic out)
+  (binding [out 0]
+    (dorun
+      (for [idx (range (count list-ints))]
+        (if (compare-around list-ints idx)
+          (set! out (+ out (nth list-ints idx))))))
+    out))
+  
--- day01/clj/test/clj/core_test.clj
+++ day01/clj/test/clj/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest captcha1
+  (testing "Part 1 of day01"
+    (is (solve-captcha1 "1122") 3)
+    (is (solve-captcha1 "1111") 4)
+    (is (solve-captcha1 "1234") 0)
+    (is (solve-captcha1 "91212129") 9)))
+
+(deftest captcha2
+  (testing "Part 2 of day02"
+    (is (solve-captcha2 "1212") 6)
+    (is (solve-captcha2 "1221") 0)
+    (is (solve-captcha2 "123425") 4)
+    (is (solve-captcha2 "123123") 12)
+    (is (solve-captcha2 "12131415") 4)))
