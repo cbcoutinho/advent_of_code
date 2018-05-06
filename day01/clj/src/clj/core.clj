ee47198a2213f70bde5ea6fea54957ef57de35bc
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-05-06 11:55:24 +0200

    Switch captchas from using bindings to using with-local-vars

--- day01/clj/src/clj/core.clj
+++ day01/clj/src/clj/core.clj
@@ -8,7 +8,9 @@
 
 (defn compare-next [list-ints idx]
   (= (nth list-ints idx)
-     (nth list-ints (mod (inc idx) (count list-ints)))))
+     (nth list-ints 
+          (mod (inc idx) 
+               (count list-ints)))))
 
 (defn compare-around [list-ints idx]
   (= (nth list-ints idx)
@@ -19,25 +21,20 @@
 (defn solve-captcha1 
   "Computes the captcha for part 1 of day01"
   [my-string]
-  (def list-ints (string-to-list-ints my-string))
-  (def ^:dynamic out)
-  (binding [out 0]
-    (dorun
-      (for [idx (range (count list-ints))]
-        (if (compare-next list-ints idx)
-          (set! out (+ out (nth list-ints idx))))))
-    out))
+  (with-local-vars [out 0
+                    list-ints (string-to-list-ints my-string)]
+    (for [idx (range (count @list-ints))]
+      (if (compare-next @list-ints idx)
+        (var-set out (+ @out (nth @list-ints idx)))))
+    @out))
  
 (defn solve-captcha2
   "Computes the captcha for part 2 of day01"
   [my-string]
-  (def list-ints
-    (string-to-list-ints my-string))
-  (def ^:dynamic out)
-  (binding [out 0]
-    (dorun
-      (for [idx (range (count list-ints))]
-        (if (compare-around list-ints idx)
-          (set! out (+ out (nth list-ints idx))))))
-    out))
+  (with-local-vars [out 0
+                    list-ints (string-to-list-ints my-string)]
+    (for [idx (range (count @list-ints))]
+      (if (compare-around @list-ints idx)
+        (var-set out (+ @out (nth @list-ints idx)))))
+    @out))
   
