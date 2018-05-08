53829d53bc31e45587c7835a8692bbbe0a095d8a
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-05-08 02:13:26 +0200

    finish part 2 of day05 in clojure

--- day05/clj/src/clj/core.clj
+++ day05/clj/src/clj/core.clj
@@ -18,16 +18,52 @@
   (assoc coll idx
          (inc (nth coll idx))))
 
+(defn dec-nth
+  "Decrement the nth element in a collection"
+  [coll idx]
+  (assoc coll idx
+         (dec (nth coll idx))))
+
 (defn count-jump-offsets
   "Count number of jump offsets required to exit a collection"
-  [coll]
-  (with-local-vars
-    [idx 0 out 0]
-    (println @idx @out)))
+  ([coll] ; Use arity to set initial conditions
+   (count-jump-offsets coll 0 0))
+  ([coll idx out]
+   (if
+     (or ; Check if index falls outside of range
+       (< idx 0) ; less than zero
+       (>= idx (count coll))) ; greater than/equal to length
+     out
+     (recur ; Call count-jump-offsets again with `updated` values
+       (inc-nth coll idx) ; Increment the coll at index idx
+       (+ idx (nth coll idx)) ; Jump with offset at index
+       (inc out))))) ; Increment number of jumps
 
+(defn count-jump-offsets2
+  "Count number of jump offsets required to exit a collection
+
+  Part 2 - decrease by one if offset is 3 or more instead of
+  incrementing"
+  ([coll]
+   (count-jump-offsets2 coll 0 0))
+  ([coll idx out]
+   (if
+     (or
+       (< idx 0)
+       (>= idx (count coll)))
+     out
+     (recur
+       (if (>= (nth coll idx) 3)
+         (dec-nth coll idx)
+         (inc-nth coll idx))
+       (+ idx (nth coll idx))
+       (inc out)))))
 
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [filename & args]
+  (println
+    (count-jump-offsets
+    ;(count-jump-offsets2
+      (parse-file filename))))
--- day05/clj/test/clj/core_test.clj
+++ day05/clj/test/clj/core_test.clj
@@ -4,6 +4,16 @@
 
 (deftest readme-test
   (testing "Tests example from README."
-    (is (= 
+    (is (=
           (count-jump-offsets [0 3 0 1 -3])
-          5))))
+          5))
+    (is (=
+          (count-jump-offsets2 [0 3 0 1 -3])
+          10))))
+
+(deftest offsets-test
+  (testing "Test offsets.txt file"
+    (is (=
+          (count-jump-offsets
+            (parse-file "../offsets.txt"))
+          376976))))
