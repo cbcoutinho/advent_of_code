4c8928af2ca7c08921ac0509998f7f7a14394d19
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-05-08 04:15:40 +0200

    Finish part 1 of day6 in clojure

--- day06/clj/src/clj/core.clj
+++ day06/clj/src/clj/core.clj
@@ -1,6 +1,57 @@
 (ns clj.core
   (:gen-class))
 
+(defn max-idx
+  "Calculates the index of the max element in a collection"
+  [coll]
+  ;(first (apply max-key second (map-indexed vector blocks)))
+  (first
+    (first
+      (filter
+        #(=
+           (second %)
+           (apply max coll))
+        (map-indexed vector coll)))))
+
+(defn alter-nth
+  [f]
+  (fn [coll idx]
+    (assoc coll idx
+           (f (nth coll idx)))))
+
+(def inc-nth (alter-nth inc))
+
+(defn redistribute
+  "Redistribute the blocks in a memory bank to the other banks of the
+  area by removing the blocks of the bank with the most blocks"
+  [coll]
+  (with-local-vars
+    [my-coll (assoc coll (max-idx coll) 0)
+     start (inc (max-idx coll))
+     end (+ (inc (max-idx coll)) (nth coll (max-idx coll)))]
+
+    (doseq [n (range @start @end)]
+      (var-set my-coll (inc-nth @my-coll (mod n (count coll)))))
+    @my-coll))
+
+(defn first-duplicate [coll]
+  "Finds the first duplicate pair of an infinite sequence. Uses
+  reduced to stop consuming sequence once duplicate is found.
+
+  https://stackoverflow.com/a/19895886/5536001"
+  (reduce (fn [acc [idx x]]
+            (if-let [v (get acc x)]
+              (reduced (conj v idx))
+              (assoc acc x [idx])))
+          {} (map-indexed #(vector % %2) coll)))
+
+(defn count-cycles
+  "Calcuates how many cycles it takes to get to a previously seen
+  state - looks for duplicates"
+  [coll]
+  (second (first-duplicate (iterate redistribute coll))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
--- day06/clj/test/clj/core_test.clj
+++ day06/clj/test/clj/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest readme-test
+  (testing "Test readme example."
+    (is (=
+          (count-cycles [0 2 7 0])
+          5))))
+
+(deftest part1-test
+  (testing "Part 1"
+    (is (=
+          (count-cycles [2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14])
+          3156))))
