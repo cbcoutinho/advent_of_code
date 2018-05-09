2898aba4a5f1d3d1d79aa1a4e4e6cf4ac9174ab4
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-05-10 01:52:46 +0200

    Add part 2 of day6 to test

--- aoc_clj/test/aoc_clj/day06_test.clj
+++ aoc_clj/test/aoc_clj/day06_test.clj
@@ -5,11 +5,17 @@
 (deftest readme-test
   (testing "Test readme example."
     (is (=
-          (count-cycles [0 2 7 0])
-          5))))
+         (count-cycles [0 2 7 0])
+         5))))
 
 (deftest part1-test
   (testing "Part 1"
     (is (=
-          (count-cycles [2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14])
-          3156))))
+         (count-cycles [2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14])
+         3156))))
+
+(deftest part2-test
+  (testing "Part 2"
+    (is (=
+         (loop-size [2 8 8 5 4 2 3 1 5 5 1 2 15 13 5 14])
+         1610))))
