004411b2644ef972f1f94e85c7104a536cc3a90a
Chris Coutinho <chrisbcoutinho@gmail.com>
2018-05-09 18:32:32 +0200

    Reformat day1

--- day01/clj/src/clj/core.clj
+++ day01/clj/src/clj/core.clj
@@ -1,24 +1,24 @@
 (ns clj.core
   (:gen-class))
 
-(defn string-to-list-ints 
+(defn string-to-list-ints
   "Convert a string of ints into a list of ints"
   [my-string]
   (map #(Character/getNumericValue %) my-string))
 
 (defn compare-next [list-ints idx]
   (= (nth list-ints idx)
-     (nth list-ints 
-          (mod (inc idx) 
+     (nth list-ints
+          (mod (inc idx)
                (count list-ints)))))
 
 (defn compare-around [list-ints idx]
   (= (nth list-ints idx)
-     (nth list-ints 
-          (mod (+ idx (/ 2 (count list-ints))) 
+     (nth list-ints
+          (mod (+ idx (/ 2 (count list-ints)))
                (count list-ints)))))
 
-(defn solve-captcha1 
+(defn solve-captcha1
   "Computes the captcha for part 1 of day01"
   [my-string]
   (with-local-vars [out 0
@@ -27,7 +27,7 @@
       (if (compare-next @list-ints idx)
         (var-set out (+ @out (nth @list-ints idx)))))
     @out))
- 
+
 (defn solve-captcha2
   "Computes the captcha for part 2 of day01"
   [my-string]
@@ -37,4 +37,3 @@
       (if (compare-around @list-ints idx)
         (var-set out (+ @out (nth @list-ints idx)))))
     @out))
-  
--- day01/clj/test/clj/core_test.clj
+++ day01/clj/test/clj/core_test.clj
@@ -4,15 +4,19 @@
 
 (deftest captcha1
   (testing "Part 1 of day01"
-    (is (solve-captcha1 "1122") 3)
-    (is (solve-captcha1 "1111") 4)
-    (is (solve-captcha1 "1234") 0)
-    (is (solve-captcha1 "91212129") 9)))
+    (let [test-cases [["1122" 3]
+                      ["1111" 4]
+                      ["1234" 0]
+                      ["91212129" 9]]]
+      (for [[value expected] test-cases]
+        (is (solve-captcha1 value) expected)))))
 
 (deftest captcha2
   (testing "Part 2 of day02"
-    (is (solve-captcha2 "1212") 6)
-    (is (solve-captcha2 "1221") 0)
-    (is (solve-captcha2 "123425") 4)
-    (is (solve-captcha2 "123123") 12)
-    (is (solve-captcha2 "12131415") 4)))
+    (let [test-cases [["1212" 6]
+                      ["1221" 0]
+                      ["123425" 4]
+                      ["123123" 12]
+                      ["12131415" 4]]]
+      (for [[value expected] test-cases]
+        (is (solve-captcha2 value) expected)))))
